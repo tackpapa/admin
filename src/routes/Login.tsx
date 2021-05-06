19c4113eef0b6e21f0922a38c84a1da4efd0c566
devtack <cance0311@gmail.com>
2021-05-06 17:13:36 +0900

    deploy

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -11,7 +11,7 @@ let add = ""
 if (process.env.NODE_ENV === "development") {
    add = "http://localhost:3001/admin";
 } else {
-    add = "http://bykerlogin.s3-website.ap-northeast-2.amazonaws.com/admin";
+    add = "https://admin.byker.io/admin";
 }
 
 function loginWithKakao() {
