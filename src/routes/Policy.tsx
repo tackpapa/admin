d893e2be26f80b0ea8161a73e7e229c8754d62d7
devtack <cance0311@gmail.com>
2021-07-02 19:56:48 +0900

    sizefix2

--- src/routes/Policy.tsx
+++ src/routes/Policy.tsx
@@ -12,7 +12,7 @@ const Policy = ()=>{
     return (
         <div className="App">
         <div id="five">
-        <iframe id="six" title="zzz" style={{width:"100%", height:"100%"}} src="https://docs.google.com/document/d/1YSNvhcOuLCRdFlyw0uuPiGpjGfpRRzx2jBsa7qRgDWU/edit?usp=sharing"></iframe>
+        <iframe id="six" title="zzz" style={{width:"100%", height:"1080px"}} src="https://docs.google.com/document/d/1YSNvhcOuLCRdFlyw0uuPiGpjGfpRRzx2jBsa7qRgDWU/edit?usp=sharing"></iframe>
       </div>
       </div>
     )
--- src/routes/Privacy.tsx
+++ src/routes/Privacy.tsx
@@ -12,7 +12,7 @@ const Privacy = ()=>{
         <div>
           <div className="App">
             <div id="five">
-               <iframe id="six" title="zzz" style={{width:"100%", height:"100%"}} src="https://docs.google.com/document/d/1xD6lHxU6ewfmoVN43LD4As-VFmsV55ah3xbXuu6xGbM/edit?usp=sharing" ></iframe>
+               <iframe id="six" title="zzz" style={{width:"100%", height:"1080px"}} src="https://docs.google.com/document/d/1xD6lHxU6ewfmoVN43LD4As-VFmsV55ah3xbXuu6xGbM/edit?usp=sharing" ></iframe>
             </div>
           </div>
         </div>
